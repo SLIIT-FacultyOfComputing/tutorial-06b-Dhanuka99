1ee5ad1be0ba0d99f44d4ed044a34523cd8d2b5f
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2022-03-27 11:24:49 +0000

    ooc concept

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,15 @@
 #include "Student.h"
 #include <iostream>
-
+#include <cstring>
 // Assign studentId and name
-Student::assignDetails() {
+void Student::assignDetails(int id,char studentName[]) {
   
+  studentId=id;
+  strcpy(name,studentName);
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display() {
+std::cout<<studentId;
+std::cout<<name;
 }
